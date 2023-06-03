b49b4aa8d6957a8efd28484aec0f529e23228b4a
Jyoti Chakma <jyotichakma.official@outlook.com>
2023-06-03 19:28:09 +0600

    core modification

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  late WebViewController _controller;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Web2App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Jum Academy', style: TextStyle(color: Colors.black),),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-          elevation: 0.0,
-        ),
-        body: Center(
-          child: WebView(
-            initialUrl: 'https://www.jumacademy.com/',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller = webViewController;
-            },
-          ),
-        ),
+      title: 'Chakma Dictionary',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: const MyHomePage(title: 'Chakma Dictionary'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late final WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://piedroid.com/chakma'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
